50537c4114269e35d08baf579b743b2a7a763c84
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-26 15:12:42 -0500

    transicion agregada

--- global.gd
+++ global.gd
@@ -1,4 +1,18 @@
 extends Node2D
 var posicionplayer = Vector2()
 var playercanmove = false
+var transition_scenetogo = ""
 
+func vineta(texto):
+	$vineta/Label.text = texto
+	$animationvineta.current_animation = "vineta"
+	
+func transition(scene):
+	transition_scenetogo = scene
+	$animationtransition.current_animation = "transition_in"
+func _on_animationtransition_animation_finished(anim_name):
+	if anim_name == "transition_in":
+		$animationtransition.current_animation = "transition_out"
+		if not transition_scenetogo == "":
+			get_tree().change_scene(transition_scenetogo)
+		
---
+++ scenes/Inicio.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_Button_button_up():
+	global.transition("res://scenes/examplescene.tscn")
