d018e14a36abcab3dd1ebbf44899594f13ae56c8
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-26 22:42:45 -0500

    fix bug de salto y animacion

--- characters/Player.gd
+++ characters/Player.gd
@@ -8,13 +8,9 @@ export var limite = 10
 export var friccion = 1.3
 export var salto = 500
 var doble_enabled = false
-var player_jumped = true
-var is_flying = true
-
+var landanim = true
 
 func _physics_process(delta):
-	
-		
 	if Input.is_action_pressed("ui_right"):
 		movimiento.x += velocidad
 		$Sprite.flip_h = true
@@ -24,36 +20,31 @@ func _physics_process(delta):
 		$Sprite.flip_h = false
 		$Sprite/ojosizquierda.flip_h = false
 		
-	if not is_on_floor():
-		is_flying = true
-	else:
-		is_flying = false
-		doble_enabled = true
-	
-	if Input.is_action_just_pressed("ui_up") and !is_on_floor() and doble_enabled:
-		movimiento.y = -(salto/1.2)
-		doble_enabled = false
-		player_jumped = true
-		is_flying = true
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		movimiento.y = -salto
-		player_jumped = true
-		is_flying = true
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			movimiento.y = -salto
+			$jumpanimations.play("jump")
+			print("jump1")
+		elif doble_enabled:
+			movimiento.y = -(salto/1.2)
+			$jumpanimations.play("jump")
+			print("jump2")
+			doble_enabled = false
 	
 	if is_on_ceiling():
 		movimiento.y = salto/4
 		
-	
-		
-	if is_flying:
-		movimiento.y += gravedad
-	else:
-		if movimiento.y > 0:
+	if is_on_floor():
+		doble_enabled = true
+		if landanim:
+			$jumpanimations.play("land")
+			landanim = false
+		if movimiento.y > gravedad:
 			movimiento.y = 0
+	else:
+		landanim = true
+		movimiento.y += gravedad
 	
 	movimiento.x /= friccion
-	
 	move_and_slide(movimiento,UP)
-	
 	global.posicionplayer = global_position
-
