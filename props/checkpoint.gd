0665773cc131067b236ab4b66a753b633172878f
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-27 12:29:06 -0500

    signal agregada a checkpoint

--- props/checkpoint.gd
+++ props/checkpoint.gd
@@ -4,6 +4,7 @@ export var checkpointID = ""
 export var music_mode = false
 export var musicpath = ""
 export var save_checkpoint = true
+signal touched
 
 func _on_Node2D_body_entered(body):
 	if save_checkpoint:
@@ -11,4 +12,5 @@ func _on_Node2D_body_entered(body):
 	if music_mode:
 		global.setsong(musicpath)
 	#global.checkpointID = checkpointID
+	emit_signal("touched")
 	queue_free()
