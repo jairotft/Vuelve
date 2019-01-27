a055b15321b0c938f08fc01bb12e6fc059c6cad9
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-27 11:45:33 -0500

    fuego arreglado 2

--- props/Fire.gd
+++ props/Fire.gd
@@ -8,6 +8,7 @@ signal character_die_with_fire
 func _ready():
 	scaley = $Sprite.scale.y
 	$Sprite.visible = FireVisible
+	$Light2D.visible = FireVisible
 func _process(delta):
 	$Sprite.scale.y = rand_range(scaley-0.2,scaley+0.2)
 	if rand_range(-2,2) > 0:
