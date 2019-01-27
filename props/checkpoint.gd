7f72d7f56b9bb8f830534521fcdcc29ff71cbb6b
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-27 12:18:50 -0500

    musica en checkpoints

--- props/checkpoint.gd
+++ props/checkpoint.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 export var checkpointID = ""
+export var music_mode = false
+export var musicpath = ""
 
 func _on_Node2D_body_entered(body):
 	global.checkpoint = global.posicionplayer
+	if music_mode:
+		global.setsong(musicpath)
 	#global.checkpointID = checkpointID
 	queue_free()
