1ff4502eebfb15c24a1f1e4487d0618123b86bd4
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-27 12:23:10 -0500

    checkpoint opcional

--- props/checkpoint.gd
+++ props/checkpoint.gd
@@ -3,9 +3,11 @@ extends Area2D
 export var checkpointID = ""
 export var music_mode = false
 export var musicpath = ""
+export var save_checkpoint = true
 
 func _on_Node2D_body_entered(body):
-	global.checkpoint = global.posicionplayer
+	if save_checkpoint:
+		global.checkpoint = global.posicionplayer
 	if music_mode:
 		global.setsong(musicpath)
 	#global.checkpointID = checkpointID
