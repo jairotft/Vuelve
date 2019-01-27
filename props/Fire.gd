788c7626eab20141e89e07d98f2591c2e400d9d4
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-27 11:30:28 -0500

    fuego animado con luz

--- props/Fire.gd
+++ props/Fire.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
 export var FireVisible= true
+var scaley = 0
 
 signal character_die_with_fire
 
 func _ready():
+	scaley = $Sprite.scale.y
 	$Sprite.visible = FireVisible
+func _process(delta):
+	$Sprite.scale.y = rand_range(scaley-0.2,scaley+0.2)
+	if rand_range(-2,2) > 0:
+		$Sprite.flip_h = true
+	else:
+		$Sprite.flip_h = false
+	
 
 
 func _on_StaticBody2D_body_entered(body):
