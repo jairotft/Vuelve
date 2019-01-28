b0a689696ac22ed663e634fafd6647e5921beaeb
leandrodreamer <leandrodreamer1@gmail.com>
2019-01-28 16:56:41 -0500

    compresion de la version final v1

--- global.gd
+++ global.gd
@@ -15,7 +15,7 @@ var volspeedout = 1.2
 var volspeedin = 1.2
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_page_up"):
+	if Input.is_action_just_pressed("fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen 
 	if transitingsong:
 		if volumentrans > volumenmin:
